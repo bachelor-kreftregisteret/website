0d6e6cc2a70b65075defc9f71720e089eb97c1ad
olagberg <s341874@oslomet.no>
2022-01-29 15:41:00 +0100

    🔧 Fixed build folder

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import css from 'rollup-plugin-css-only';
+import run from '@jamesives/github-pages-deploy-action'
 
 const production = !process.env.ROLLUP_WATCH;
 
