67cff66e4ae3cf3ef578e543a822bd04e48dbd6c
olagberg <s341874@oslomet.no>
2022-01-29 16:51:27 +0100

    🔧 Try to add "deploy" to package.json

--- rollup.config.js
+++ rollup.config.js
@@ -73,6 +73,7 @@ export default {
 
 		// Deploy
 		run({
+			token: process.env['ACCESS_TOKEN'],
 			branch: 'gh-pages',
 			folder: 'public',
 			repositoryName: 'bachelor-kreftregisteret/website',
